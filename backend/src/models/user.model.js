10b60e14bafb4df4f947cd7fe83edaaa07947675
Arya10n <roytintin1603@gmail.com>
2025-04-10 04:17:32 +0530

    added models, routes, controllers

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,26 @@
+import { Schema } from 'mongoose';
+import mongoose from 'mongoose';
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+    },
+    savedPosts: {
+      type: [String],
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model('User', userSchema);
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,31 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './lib/db.js';
+
+import postRouter from './routes/post.route.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
+app.use(express.json());
+
+// app.use('/users', userRouter);
+app.use('/posts', postRouter);
+// app.use('/comments', commentRouter);
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+
+  res.json({
+    message: error.message || 'Something went wrong!',
+    status: error.status,
+    stack: error.stack,
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  connectDB();
 });
